4f25be4010c1d634ea0c9da216e001fdb02cd790
malits <maxlitster@gmail.com>
2021-01-25 11:27:21 -0800

    more linops stuff

--- bartpy/linops/linop.py
+++ bartpy/linops/linop.py
@@ -1,7 +1,11 @@
 import numpy as np
 
+from .linop_swig import plus, chain, stack, domain, codomain
+# FIXME: Clean up these imports, modify namespace in SWIG
 from .linop_swig import forward as _forward
-from .linop_swig import plus as _plus
+from .linop_swig import adjoint as _adjoint
+from .linop_swig import normal as _normal
+from .linop_swig import pseudo_inv as _pseudo_inv
 
 from ..utils.md_utils import expand_array, expand_dims
 
@@ -11,20 +15,45 @@ def forward(op, dest_dims, src_arr):
 
     Apply linear operator
     :param op: Linear operator
-    :src_arr: source_numpy_array
+    :dest_dims: Output dimensions
+    :src_arr: source numpy array
     """
     dst_dim = expand_dims(dest_dims)
     src = expand_array(src_arr)
 
     return _forward(op, dst_dim, src).squeeze()
 
-def plus(op_a, op_b):
+def adjoint(op, dest_dims, src_arr):
     """
-    Add two linear operators
-
-    :param op_a: First operator
-    :param op_b: Second operator
-
-    :returns the sum of the two operators:
+    Apply adjoint
+    :param op: Linear Operator
+    :dest_dims: Output dimensions
+    :src_arr: source numpy array
     """
-    return _plus(op_a, op_b)
+    dst_dim = expand_dims(dest_dims)
+    src = expand_array(src_arr)
+
+    return _adjoint(op, dst_dim, src).squeeze()
+
+def normal(op, dest_dims, src_arr):
+    """
+    :param op: Linear Operator
+    :dest_dims: Output dimensions
+    :src_arr: 
+    """
+    dst_dim = expand_dims(dest_dims)
+    src = expand_array(src_arr)
+
+    return _normal(op, dst_dim, src).squeeze()
+
+def pseudo_inv(op, llambda, dest_dims, src_arr):
+    """
+    :param op: Linear Operator
+    :dest_dims: Output dimensions
+    :src_arr: 
+    :llambda: lambda term
+    """
+    dst_dim = expand_dims(dest_dims)
+    src = expand_array(src_arr)
+
+    return _pseudo_inv(op, llambda, dst_dim, src)
